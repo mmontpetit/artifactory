f6d2212afcfd6a86fc5f40aa7ecc791e6c468b77
Micheal Montpetit <micheal.montpetit@qlik.com>
2019-09-14 15:23:02 +0200

    ..

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $INPUT_WHO_TO_GREET"
 time=$(date)
 echo ::set-output name=time::$time
 echo "$ARTIFACTORY_PASSWORD" | docker login "$ARTIFACTORY_REGISTRY_URL" -u "$ARTIFACTORY_USERNAME" --password-stdin
