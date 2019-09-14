420f559bd1b4cb052ba59a582d96313e20104bf8
Micheal Montpetit <micheal.montpetit@qlik.com>
2019-09-15 00:26:23 +0200

    ..

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
 time=$(date)
-echo ::set-output name=time::$time
-echo "$INPUT_ARTIFACTORY_PASSWORD" | docker login "$INPUT_ARTIFACTORY_DOCKER_URL" -u "$INPUT_ARTIFACTORY_USERNAME" --password-stdin
+docker login "$INPUT_ARTIFACTORY_DOCKER_URL" -u "$INPUT_ARTIFACTORY_USERNAME" -p "$INPUT_ARTIFACTORY_PASSWORD"
 
 
